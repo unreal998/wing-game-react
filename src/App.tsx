6714bba407de87ef740286b3128b19a82a5d3929
SOLONYI aNDRII <asfodel1975@gmail.com>
2025-02-28 12:19:01 +0200

    Footer new

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { AppBar } from "@mui/material";
+import { AppBar, Box } from "@mui/material";
 import { Home } from "./modules/Home";
 import { MAIN_COLORS } from "./shared/colors";
 import Footer from "./modules/Footer";
 
 const App = () => {
   return (
-    <AppBar sx={{ backgroundColor: MAIN_COLORS.mainBG }}>
-      <Footer />
+    <AppBar sx={{ height: "100%", backgroundColor: MAIN_COLORS.mainBG }}>
+      <Box sx={{ position: "fixed", bottom: 0, left: 0, width: "100%" }}>
+        <Footer />
+      </Box>
     </AppBar>
   );
 };
