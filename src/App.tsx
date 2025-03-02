8c94e9fe23918bbe498f28254a5ea2bae023215f
SOLONYI aNDRII <asfodel1975@gmail.com>
2025-03-02 17:27:26 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Box } from "@mui/material";
 import { Home } from "./modules/Home";
 
-
 const App = () => {
-
   return (
-    <Box sx={{ width: "100vw", height: "100vh", position: "relative", overflow: "hidden" }}>
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
       <Home />
     </Box>
   );
