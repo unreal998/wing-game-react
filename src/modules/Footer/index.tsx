c108e9f08be9b0d912d3d7ba786d4d178d399c8a
SOLONYI aNDRII <asfodel1975@gmail.com>
2025-03-03 10:45:14 +0200

    wip

--- src/modules/Footer/index.tsx
+++ src/modules/Footer/index.tsx
@@ -6,27 +6,15 @@ import Mission from "../../assets/mission.svg";
 import Wind from "../../assets/wind.svg";
 import Cart from "../../assets/cart-shopping.svg";
 import Wallet from "../../assets/wallet.svg";
+import { StyledFooterBoxes } from "./componets/StyledFooterBoxes";
+import { StyledFooterBoxesTypography } from "./componets/StyledFooterBoxesTypography";
+import { StyledCenterFooter } from "./componets/StyledCenterFooter";
+import { StyledFooterBox } from "./componets/StyledFooterBox";
 
 const Footer = () => {
   return (
-    <Box
-      sx={{
-        height: "91.5px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "7px",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1,
-        }}
-      >
+    <StyledFooterBox>
+      <StyledFooterBoxes>
         <img
           src={Linked}
           alt="linked"
@@ -41,103 +29,30 @@ const Footer = () => {
         >
           Referal
         </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "7px",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1,
-        }}
-      >
+      </StyledFooterBoxes>
+      <StyledFooterBoxes>
         <img src={Mission} alt="mission" />
-        <Typography
-          sx={{
-            color: MAIN_COLORS.footerText,
-            fontSize: "12px",
-            fontWeight: 400,
-          }}
-        >
-          Mission
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          gap: "7px",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1,
-          border: `1px solid  ${MAIN_COLORS.contentYellow}`,
-          borderBottom: "none",
-          borderTopLeftRadius: "52%",
-          borderTopRightRadius: "52%",
-        }}
-      >
+        <StyledFooterBoxesTypography>Mission</StyledFooterBoxesTypography>
+      </StyledFooterBoxes>
+      <StyledCenterFooter>
         <img src={Wind} alt=" wind " />
-        <Typography
-          sx={{
-            color: MAIN_COLORS.witeText,
-            fontSize: "12px",
-            fontWeight: 400,
-          }}
-        >
-          Home
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "7px",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1,
-        }}
-      >
+        <StyledFooterBoxesTypography>Home</StyledFooterBoxesTypography>
+      </StyledCenterFooter>
+      <StyledFooterBoxes>
         <img src={Cart} alt="cart" />
-        <Typography
-          sx={{
-            color: MAIN_COLORS.footerText,
-            fontSize: "12px",
-            fontWeight: 400,
-          }}
-        >
-          Shop
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "7px",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1,
-        }}
-      >
+        <StyledFooterBoxesTypography>Shop</StyledFooterBoxesTypography>
+      </StyledFooterBoxes>
+      <StyledFooterBoxes>
         <img
           src={Wallet}
           alt=" wallet"
           style={{ marginBottom: "5px", paddingTop: "5px" }}
         />
-        <Typography
-          sx={{
-            color: MAIN_COLORS.footerText,
-            fontSize: "12px",
-            fontWeight: 400,
-          }}
-        >
-          Wallet
-        </Typography>
-      </Box>
+        <StyledFooterBoxesTypography>Wallet</StyledFooterBoxesTypography>
+      </StyledFooterBoxes>
       <Box></Box>
       <Box></Box>
-    </Box>
+    </StyledFooterBox>
   );
 };
 export default Footer;
