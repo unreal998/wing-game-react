f7c5bb55d206b9e3acd3f5428ee61f77cb92ef20
Maks <unreal9981@gmail.com>
2025-03-10 00:53:04 +0200

    wip

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,6 +1,6 @@
 import WebApp from "@twa-dev/sdk";
 
 const HEADER_AND_FOOTER_SIZE = 167;
-alert(WebApp.viewportHeight);
+alert(WebApp.viewportStableHeight);
 export const heightProportion =
-  (WebApp.viewportHeight || window.innerHeight) - HEADER_AND_FOOTER_SIZE;
+  (WebApp.viewportStableHeight || window.innerHeight) - HEADER_AND_FOOTER_SIZE;
