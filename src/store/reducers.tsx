7ed3a862fa5f319f29649154027fbca4a93b846f
Maks <unreal9981@gmail.com>
2025-03-20 23:35:27 +0200

    saga added

--- src/store/reducers.tsx
+++ src/store/reducers.tsx
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import { homeSlice } from "../modules/Home/slices";
+import { headerSlice } from "../modules/Header/slices";
 
-const slices: any[] = [homeSlice];
+const slices: any[] = [homeSlice, headerSlice];
 
 const toolkitReducers = Object.fromEntries(
   slices.map(({ name, reducer }) => [name, reducer]),
--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,5 +1,6 @@
 import { all } from "redux-saga/effects";
+import { watchHeaderActions } from "../modules/Header/sagas";
 
 export function* rootSaga() {
-  yield all([]);
+  yield all([watchHeaderActions()]);
 }
