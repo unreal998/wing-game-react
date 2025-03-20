e452c9e136444e12d514ebe4b03f30056297dbab
SOLONYI aNDRII <asfodel1975@gmail.com>
2025-03-20 16:33:33 +0200

    planet screen init

---
+++ src/modules/Planet/components/StyledButtonGame.ts
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material";
+import { ButtonGame } from "../../../shared/ButtonGame";
+import { MAIN_COLORS } from "../../../shared/colors";
+
+export const StyledButtonGame = styled(ButtonGame)({
+  fontSize: "16px",
+  fontWeight: 900,
+  textTransform: "capitalize",
+  color: MAIN_COLORS.textColor,
+});
---
+++ src/modules/Planet/components/StyledPlanetBox.ts
@@ -0,0 +1,9 @@
+import { Box, styled } from "@mui/material";
+
+export const StyledPlanetBox = styled(Box)({
+  marginTop: "-200px",
+  display: "grid",
+  gap: "10px",
+  paddingLeft: "14px",
+  paddingRight: "14px",
+});
