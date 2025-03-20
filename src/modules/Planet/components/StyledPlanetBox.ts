f2a439c2f12848e5dc48b72e5af3e02bf04c21e8
SOLONYI aNDRII <asfodel1975@gmail.com>
2025-03-20 22:27:24 +0200

    wip

--- src/modules/Planet/components/StyledPlanetBox.ts
+++ src/modules/Planet/components/StyledPlanetBox.ts
@@ -1,14 +1,14 @@
 import { Box, styled } from "@mui/material";
 
 export const StyledPlanetBox = styled(Box)({
-  marginTop: "-130px",
+  marginTop: "-150px",
   display: "flex",
   gap: "10px",
   paddingLeft: "14px",
   paddingRight: "14px",
   flexWrap: "wrap",
   flexDirection: "row",
-  width: "100%",
+
   alignItems: "center",
   justifyContent: "center",
 });
--- src/modules/Planet/index.tsx
+++ src/modules/Planet/index.tsx
@@ -50,7 +50,7 @@ export const Planet = () => {
   );
 
   return (
-    <Box sx={{ width: "100%", height: "80vh" }}>
+    <Box sx={{ width: "100%", height: "80vh", marginTop: "-130px" }}>
       <Canvas
         camera={{
           position: [9, 2, 20],
