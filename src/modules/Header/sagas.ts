69c1d60dd6fa6603579294f19789aa1ab59fe71a
SOLONYI aNDRII <asfodel1975@gmail.com>
2025-03-23 15:20:53 +0200

    Button "copy" in wallet

--- src/modules/Header/sagas.ts
+++ src/modules/Header/sagas.ts
@@ -2,11 +2,15 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import { initAction, initActionFailure, initActionSuccess } from "./slices";
 import { UserData, UserInitData } from "../../shared/types";
 import { fetchInitData } from "./api";
+import { createWalletActionSuccess } from "../Wallet/slices";
 
 function* handleInit(action: { type: string; payload: UserInitData }) {
   try {
     const userData: UserData = yield call(fetchInitData, action.payload);
+
     yield put(initActionSuccess(userData));
+
+    yield put(createWalletActionSuccess(userData.wallet));
   } catch (err: any) {
     yield put(initActionFailure(err.toString()));
   }
