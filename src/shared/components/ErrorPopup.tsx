b5b4a73fa27357bd64f084f091df677f5030324e
NikitaGavr <nikita2gavrilenko@gmail.com>
2025-04-02 18:35:54 +0200

    refactored style to MUI components

--- src/shared/components/ErrorPopup.tsx
+++ src/shared/components/ErrorPopup.tsx
@@ -1,33 +1,71 @@
 import { useSelector } from "react-redux";
 import { selectErrors } from "./selectors";
 import { useState } from "react";
+import { Alert, Button, Snackbar } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledAlert = styled(Alert)(({ theme }) => ({
+  backgroundColor: "#1a237e", // Dark blue background
+  color: "#ffffff", // White text
+  maxWidth: "300px",
+  borderLeft: "4px solid #ff5252", // Red accent border
+  "& .MuiAlert-icon": {
+    color: "#ff5252", // Red icon
+  },
+  "& .MuiAlert-action": {
+    paddingLeft: "16px",
+  },
+}));
 
 const ErrorPopup = () => {
   const errors = useSelector(selectErrors);
-  const [toggle, setToggle] = useState(true);
+  errors.push("5.44.33");
+  const [open, setOpen] = useState(true);
   const activeErrors = errors.filter(
     (error: string) => error[0] === "5" || error[0] === "4",
   );
-  if (activeErrors.length === 0 || !toggle) {
+  const handleClose = () => setOpen(false);
+  if (activeErrors.length === 0 || !open) {
     return null;
   }
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        position: "fixed",
-        top: "30px",
-        right: "20px",
-        border: "1px solid #ef9a9a",
-        padding: "15px",
-        borderRadius: "4px",
-        maxWidth: "300px",
+    <Snackbar
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      sx={{
+        marginTop: "30px",
+        "& .MuiPaper-root": {
+          borderRadius: "8px",
+        },
       }}
     >
-      Opps, something went wrong {activeErrors[0]}
-      <button onClick={() => setToggle(false)}>OK</button>
-    </div>
+      <StyledAlert
+        severity="error"
+        action={
+          <Button
+            color="inherit"
+            size="small"
+            onClick={handleClose}
+            sx={{
+              color: "#ffffff",
+              border: "1px solid rgba(255, 255, 255, 0.3)",
+              borderRadius: "4px",
+              padding: "2px 8px",
+              "&:hover": {
+                backgroundColor: "rgba(255, 255, 255, 0.1)",
+              },
+            }}
+          >
+            OK
+          </Button>
+        }
+      >
+        Oops, something went wrong: {activeErrors[0]}
+      </StyledAlert>
+    </Snackbar>
   );
 };
 
