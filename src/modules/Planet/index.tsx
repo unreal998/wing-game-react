f384de0123708d200b7d7f2ef51598121b74bd29
Roman Stakhiv <Roma.Stakhiv@gmail.com>
2025-04-28 22:35:44 +0300

    add modules 1-4

--- src/modules/Planet/index.tsx
+++ src/modules/Planet/index.tsx
@@ -1,18 +1,26 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { StyledPlanetBox } from "./components/StyledPlanetBox";
 import { StyledPlanetButton } from "./components/StyledPlanetButton";
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSelectedCountry } from "../Home/slices";
 import { selectAreasData, selectCountiresData } from "../Header/selectors";
 import { AreaType } from "../../shared/types";
+import { MAIN_COLORS } from "../../shared/colors";
+import ModuleTopText from "./components/ModuleTopText";
+import ModalBox from "./components/ModalBox";
+import ModuleBottomText from "./components/ModuleBottomText";
 
 export const Planet = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const areasData = useSelector(selectAreasData());
   const countries = useSelector(selectCountiresData());
+
+  const [showModuleOne, setShowModuleOne] = useState(true);
+  const [showModuleTwo, setShowModuleTwo] = useState(false);
+  const [showModuleThree, setShowModuleThree] = useState(false);
 
   const handleButtonPress = useCallback(
     (selectedCountry: AreaType) => {
@@ -21,6 +29,16 @@ export const Planet = () => {
     },
     [dispatch, navigate],
   );
+
+  const handleModuleClick = useCallback(() => {
+    if (showModuleOne) {
+      setShowModuleOne(false);
+      setShowModuleTwo(true);
+    } else if (showModuleTwo) {
+      setShowModuleTwo(false);
+      setShowModuleThree(true);
+    }
+  }, [showModuleOne, showModuleTwo]);
 
   const userCountiresData = useMemo(() => {
     if (!countries || !areasData) return [];
@@ -53,10 +71,22 @@ export const Planet = () => {
         height: "80vh",
         marginTop: "-10px",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        opacity: 0,
+        animation: "fadeIn 1s forwards",
+        "@keyframes fadeIn": {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
       }}
+      onClick={handleModuleClick}
     >
+      {(showModuleTwo || showModuleThree) && (
+        <ModuleTopText showModule={showModuleThree} />
+      )}
+
       <StyledPlanetBox>
         {userCountiresData &&
           userCountiresData?.length &&
@@ -65,14 +95,43 @@ export const Planet = () => {
               key={country.name}
               sx={{
                 ...getCoords(index),
+                ...(showModuleThree && {
+                  boxShadow: `0 0 10px ${MAIN_COLORS.activeTabColor}`,
+                  animationName: "pulseShadow",
+                  animationDuration: "2s",
+                  animationTimingFunction: "ease-in-out",
+                  animationIterationCount: "infinite",
+                  "@keyframes pulseShadow": {
+                    "0%": {
+                      boxShadow: `0 0 10px ${MAIN_COLORS.activeTabColor}`,
+                    },
+                    "50%": {
+                      boxShadow: `0 0 60px ${MAIN_COLORS.activeTabColor}`,
+                    },
+                    "100%": {
+                      boxShadow: `0 0 10px ${MAIN_COLORS.activeTabColor}`,
+                    },
+                  },
+                }),
+                "&.Mui-disabled": {
+                  backgroundColor: "rgb(134 134 134)",
+                  boxShadow: "none",
+                  animation: "none",
+                },
               }}
               disabled={!country.available}
-              onClick={() => handleButtonPress(country)}
+              onClick={() => {
+                if (showModuleThree) {
+                  handleButtonPress(country);
+                }
+              }}
             >
               {country.title}
             </StyledPlanetButton>
           ))}
       </StyledPlanetBox>
+      {showModuleTwo && <ModuleBottomText />}
+      {showModuleOne && <ModalBox onClick={handleModuleClick} />}
     </Box>
   );
 };
