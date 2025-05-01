820f5b7cd2141808ef06258d84af3502a378386e
Maks <unreal9981@gmail.com>
2025-05-01 21:28:56 +0300

    zIndex

--- src/App.tsx
+++ src/App.tsx
@@ -96,7 +96,7 @@ const App = () => {
     >
       <Header />
       <ErrorPopup />
-      <Box sx={{ flexGrow: 1, zIndex: 100 }}>
+      <Box sx={{ flexGrow: 1, zIndex: 99 }}>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Planet />} />
