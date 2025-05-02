ae568ba9a462da543cd759150dc477ae999d2de4
Maks <unreal9981@gmail.com>
2025-05-02 03:34:46 +0300

    headers added

--- src/modules/Wallet/api.ts
+++ src/modules/Wallet/api.ts
@@ -2,12 +2,20 @@ import axios from "axios";
 import { SERVER_URL } from "../../shared/constants";
 
 export const fetchCreateWallet = async (uid: string) => {
-  const response = await axios.post(`${SERVER_URL}/wallet/create?uid=${uid}`);
+  const response = await axios.post(`${SERVER_URL}/wallet/create?uid=${uid}`, {
+    headers: {
+      "ngrok-skip-browser-warning": true,
+    },
+  });
   return response.data;
 };
 
 export const fetchWithdrawData = async (uid: string) => {
-  const response = await axios.get(`${SERVER_URL}/withdraw?uid=${uid}`);
+  const response = await axios.get(`${SERVER_URL}/withdraw?uid=${uid}`, {
+    headers: {
+      "ngrok-skip-browser-warning": true,
+    },
+  });
   return response.data.data;
 };
 
@@ -18,14 +26,22 @@ export const sendWithdrawRequest = async (
   tonMemo: string,
 ) => {
   const tid = "123456789";
-  const response = await axios.post(`${SERVER_URL}/withdraw`, {
-    uid,
-    wallet,
-    sum: parseFloat(amount),
-    MEMO: tonMemo,
-    status: "new",
-    tid,
-  });
+  const response = await axios.post(
+    `${SERVER_URL}/withdraw`,
+    {
+      uid,
+      wallet,
+      sum: parseFloat(amount),
+      MEMO: tonMemo,
+      status: "new",
+      tid,
+    },
+    {
+      headers: {
+        "ngrok-skip-browser-warning": true,
+      },
+    },
+  );
 
   return response.data;
 };
