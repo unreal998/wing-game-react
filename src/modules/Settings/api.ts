3e2d7a1c72ddb9c51b3e0bcb0c215759219d197c
Maksym Solonyi <unreal9981@gmail.com>
2025-05-14 03:09:28 +0300

    headers

--- src/modules/Settings/api.ts
+++ src/modules/Settings/api.ts
@@ -5,11 +5,20 @@ export async function updateUserSettings(uid: string, settings: object) {
   const response = await axios.post(
     `${SERVER_URL}/user/settings?uid=${uid}`,
     settings,
+    {
+      headers: {
+        "ngrok-skip-browser-warning": true,
+      },
+    },
   );
   return response.data;
 }
 
 export async function fetchGetRoadmap(lng: string) {
-  const response = await axios.get(`${SERVER_URL}/roadmap?lng=${lng}`);
+  const response = await axios.get(`${SERVER_URL}/roadmap?lng=${lng}`, {
+    headers: {
+      "ngrok-skip-browser-warning": true,
+    },
+  });
   return response.data;
 }
