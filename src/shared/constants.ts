4f4b8fa6ad925e0efcf3ab40a017383c73373e81
Roman Stakhiv <Roma.Stakhiv@gmail.com>
2025-05-15 19:01:38 +0300

    errHandlerLogic

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,6 +1,6 @@
-// export const SERVER_URL = "http://localhost:3003";
+export const SERVER_URL = "http://localhost:3003";
 // prod
-export const SERVER_URL = "https://b90e-95-164-85-150.ngrok-free.app";
+// export const SERVER_URL = "https://b90e-95-164-85-150.ngrok-free.app";
 
 export const USER_MOCK_TELEGRAM_DATA = {
   telegramID: 123456789,
